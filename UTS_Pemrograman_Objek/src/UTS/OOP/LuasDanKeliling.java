13be918193dd6caf2ec2db8024feed686ef08142
jekitut <adklmn.irul@gmail.com>
2020-11-11 14:14:11 +0700

    UTS 3A

---
+++ UTS_Pemrograman_Objek/src/UTS/OOP/LuasDanKeliling.java
@@ -0,0 +1,19 @@
+package UTS.OOP;
+
+public class LuasDanKeliling {
+    
+    public static void main(String[] args) {
+        int sisi;
+        int hasilLuas;
+        int hasilKeliling;
+        
+        // NIM = 201969040035
+        sisi = 35;
+        hasilLuas = sisi * sisi;
+        hasilKeliling = 4 * sisi;
+        
+        System.out.println("Hasil luas persegi adalah : " + hasilLuas);
+        System.out.println("Hasil keliling persegi adalah : " + hasilKeliling);
+    }
+    
+}
